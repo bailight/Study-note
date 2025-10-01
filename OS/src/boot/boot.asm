46eb4c045dba99e4a0de3436baf044113e35689e
Baili <1172177657@qq.com>
2025-10-01 16:48:52 +0300

    for test 10.01

---
+++ OS/src/boot/boot.asm
@@ -0,0 +1,25 @@
+[org 0x7c00]
+
+start:
+    mov ax, 0x03
+    int 0x10
+
+    mov si, msg
+    call print
+    jmp $
+
+print:
+    mov ah, 0x0e
+    .nc:
+        lodsb
+        cmp al, 0
+        je .done
+        int 0x10
+        jmp .nc
+    .done:
+        ret
+ 
+msg db 'Hello, World!', 0
+
+times 510 - ($ - $$) db 0
+dw 0xaa55
