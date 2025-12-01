25807ed57c133d8a14f58b4d4dbd93937158461f
Baili <1172177657@qq.com>
2025-12-01 15:58:53 +0300

    12.01

--- OS/OS/src/kernel/memory.h
+++ OS/OS/src/kernel/memory.h
@@ -7,35 +7,71 @@
 #define E820_COUNT_ADDR (E820_BUF - 4)
 #define E820_MAX_ENTRIES 32
 
-#define PAGE_2M_SHIFT    21
-#define PAGE_2M_SIZE     (1UL << PAGE_2M_SHIFT)
-#define PAGE_2M_MASK     (~(PAGE_2M_SIZE - 1))
-#define PAGE_2M_ALIGN(addr)  ((addr + PAGE_2M_SIZE - 1) & PAGE_2M_MASK)
+#define PTRS_PER_PAGE	512
 
-#define PHY_TO_VIRT(phy) (0xFFFF800000000000UL | (phy))
+#define PAGE_OFFSET	((unsigned long)0xffff800000000000)
 
-#define PAGE_FREE        0x00
-#define PAGE_USED        0x01
-#define PAGE_RESERVED    0x02
+#define PAGE_GDT_SHIFT	39
+#define PAGE_1G_SHIFT	30
+#define PAGE_2M_SHIFT	21
+#define PAGE_4K_SHIFT	12
 
-#define ZONE_NORMAL      0x01
+#define PAGE_2M_SIZE	(1UL << PAGE_2M_SHIFT)
+#define PAGE_4K_SIZE	(1UL << PAGE_4K_SHIFT)
 
-#define PGD_BASE 0xFFFF800000100000
-#define PUD_BASE 0xFFFF800000101000
-#define PMD_BASE 0xFFFF800000102000
+#define PAGE_2M_MASK	(~ (PAGE_2M_SIZE - 1))
+#define PAGE_4K_MASK	(~ (PAGE_4K_SIZE - 1))
 
-#define PAGE_PRESENT  (1 << 0)
-#define PAGE_WRITE    (1 << 1)
-#define PAGE_USER     (1 << 2)
-#define PAGE_PSE      (1 << 7)
-#define PAGE_FLAGS    (PAGE_PRESENT | PAGE_WRITE | PAGE_PSE)
+#define PAGE_2M_ALIGN(addr)	(((unsigned long)(addr) + PAGE_2M_SIZE - 1) & PAGE_2M_MASK)
+#define PAGE_4K_ALIGN(addr)	(((unsigned long)(addr) + PAGE_4K_SIZE - 1) & PAGE_4K_MASK)
 
-static int bits_map_inited = 0;
+#define Virt_To_Phy(addr)	((unsigned long)(addr) - PAGE_OFFSET)
+#define Phy_To_Virt(addr)	((unsigned long *)((unsigned long)(addr) + PAGE_OFFSET))
 
-static volatile int page_lock = 0;
+#define Virt_To_2M_Page(kaddr)	(memory_management_struct.pages_struct + (Virt_To_Phy(kaddr) >> PAGE_2M_SHIFT))
+#define Phy_to_2M_Page(kaddr)	(memory_management_struct.pages_struct + ((unsigned long)(kaddr) >> PAGE_2M_SHIFT))
 
-#define LOCK()   while(__sync_lock_test_and_set(&page_lock, 1)) {}
-#define UNLOCK() __sync_lock_release(&page_lock, 0);
+#define PAGE_XD		(unsigned long)0x1000000000000000
+#define	PAGE_PAT	(unsigned long)0x1000
+#define	PAGE_Global	(unsigned long)0x0100
+#define	PAGE_PS		(unsigned long)0x0080
+#define	PAGE_Dirty	(unsigned long)0x0040
+#define	PAGE_Accessed	(unsigned long)0x0020
+#define PAGE_PCD	(unsigned long)0x0010
+#define PAGE_PWT	(unsigned long)0x0008
+#define	PAGE_U_S	(unsigned long)0x0004
+#define	PAGE_R_W	(unsigned long)0x0002
+#define	PAGE_Present	(unsigned long)0x0001
+#define PAGE_KERNEL_GDT		(PAGE_R_W | PAGE_Present)	
+#define PAGE_KERNEL_Dir		(PAGE_R_W | PAGE_Present)
+#define	PAGE_KERNEL_Page	(PAGE_PS  | PAGE_R_W | PAGE_Present)
+#define PAGE_USER_Dir		(PAGE_U_S | PAGE_R_W | PAGE_Present)
+#define	PAGE_USER_Page		(PAGE_PS  | PAGE_U_S | PAGE_R_W | PAGE_Present)
+
+typedef struct {unsigned long pml4t;} pml4t_t;
+#define	mk_mpl4t(addr,attr)	((unsigned long)(addr) | (unsigned long)(attr))
+#define set_mpl4t(mpl4tptr,mpl4tval)	(*(mpl4tptr) = (mpl4tval))
+
+
+typedef struct {unsigned long pdpt;} pdpt_t;
+#define mk_pdpt(addr,attr)	((unsigned long)(addr) | (unsigned long)(attr))
+#define set_pdpt(pdptptr,pdptval)	(*(pdptptr) = (pdptval))
+
+
+typedef struct {unsigned long pdt;} pdt_t;
+#define mk_pdt(addr,attr)	((unsigned long)(addr) | (unsigned long)(attr))
+#define set_pdt(pdtptr,pdtval)		(*(pdtptr) = (pdtval))
+
+
+typedef struct {unsigned long pt;} pt_t;
+#define mk_pt(addr,attr)	((unsigned long)(addr) | (unsigned long)(attr))
+#define set_pt(ptptr,ptval)		(*(ptptr) = (ptval))
+
+struct Page {
+    unsigned long phy_addr;  // 物理页地址
+    unsigned int ref_count;  // 引用计数
+    struct Page *next;       // 用于空闲链表
+};
 
 struct E820{
 	unsigned long address;
@@ -49,50 +85,7 @@ struct Global_Memory_Descriptor {
     volatile struct E820 e820[32];
     volatile unsigned long e820_length;
 
-	unsigned long * bits_map;
-	volatile unsigned long 	bits_size;
-	volatile unsigned long   bits_length;
-
-	struct Page *	pages_struct;
-	volatile unsigned long	pages_size;
-	volatile unsigned long 	pages_length;
-
-	struct Zone * 	zones_struct;
-	volatile unsigned long	zones_size;
-	volatile unsigned long 	zones_length;
-
 	volatile unsigned long 	start_code , end_code , end_data , end_brk;
-
-	unsigned long	end_of_struct;
-};
-
-struct Page
-{
-	struct Zone *	zone_struct;
-	volatile unsigned long	PHY_address;
-	volatile unsigned long	attribute;
-
-	volatile unsigned long	reference_count;
-	
-	volatile unsigned long	age;
-};
-
-struct Zone
-{
-	struct Page * 	pages_group;
-	volatile unsigned long	pages_length;
-	
-	volatile unsigned long	zone_start_address;
-	volatile unsigned long	zone_end_address;
-	volatile unsigned long	zone_length;
-	volatile unsigned long	attribute;
-
-	struct Global_Memory_Descriptor * GMD_struct;
-
-	volatile unsigned long	page_using_count;
-	volatile unsigned long	page_free_count;
-
-	volatile unsigned long	total_pages_link;
 };
 
 void init_memory(void);
@@ -101,7 +94,6 @@ void init_memory_map(void);
 struct Page * alloc_page(void);
 void free_page(struct Page *page);
 int map_page(unsigned long virt_addr, unsigned long phy_addr);
-void unmap_page(unsigned long virt_addr);
 void *malloc(unsigned long size);
 void free(void *ptr);
 
